f3b524ccbfa1a45779393cda8c51fe7b93bf5ca8
Franciscoandrade413 <francsicoandrade2001@gmail.com>
2021-08-10 13:04:02 -0700

    commit tree cdc64cdf3dcf61466151b09b387cf26f5c0797b0

--- lib/example.rb
+++ lib/example.rb
@@ -28,11 +28,14 @@ class Plant
 end
 
 lawn = Garden.new(name: 'Front Lawn')
+# we can call Garden.new because Garden is defined above
 
 basil = Plant.new(name: 'Basil')
+# we can call Plant.new because Plant is defined above
 basil.garden = lawn
 
 cucumber = Plant.new(name: 'Cucumber')
 cucumber.garden = lawn
+# we can associate a plant and a garden because both are defined above
 
 p lawn.plants
